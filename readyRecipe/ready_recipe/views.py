44c1138d3f9cad46e96806c05bc84453ecb1d3fb
gflouriz <georgeflouris98@gmail.com>
2020-04-02 05:00:19 +0100

    cookie fix 2

--- readyRecipe/ready_recipe/views.py
+++ readyRecipe/ready_recipe/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,render_to_response
 from django.contrib.auth import authenticate, login,logout
 from django.contrib import messages
 from django.contrib.auth import update_session_auth_hash
@@ -16,24 +16,35 @@ from django.urls import reverse
 from django.template.defaultfilters import slugify
 from datetime import datetime
 
-def get_server_side_cookie(request, cookie, default_val=0):
+
+# A helper method
+def get_server_side_cookie(request, cookie, default_val=None):
     val = request.session.get(cookie)
     if not val:
         val = default_val
     return val
 
-
 def visitor_cookie_handler(request):
-    visits = int(get_server_side_cookie(request, 'visits', '1'))
-    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
-    last_visit_time = datetime.strptime(last_visit_cookie[:-7], "%Y-%m-%d %H:%M:%S")
 
-    if (datetime.now() - last_visit_time).seconds > 0:
-        visits += 1
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+
+    last_visit_cookie = get_server_side_cookie(request,
+                                               'last_visit',
+                                               str(datetime.now()))
+
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        # Update the last visit cookie now that we have updated the count
         request.session['last_visit'] = str(datetime.now())
     else:
-        visits = 1
+        # Set the last visit cookie
         request.session['last_visit'] = last_visit_cookie
+
+    # Update/set the visits cookie
     request.session['visits'] = visits
 
 
@@ -44,8 +55,10 @@ def index(request):
     context_dict['categories'] = category_list
 
     visitor_cookie_handler(request)
-    context_dict['visits'] = request.session['visits']
+    # Obtain our Response object early so we can add cookie information.
     response = render(request, 'ready_recipe/index.html', context=context_dict)
+
+    # Return response back to the user, updating any cookies that need changed.
     return response
 
 
