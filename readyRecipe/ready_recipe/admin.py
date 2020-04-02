1f7107de0b22dcf228ef88982e0756fef2146d97
gflouriz <georgeflouris98@gmail.com>
2020-04-02 05:17:54 +0100

    tweaks

--- readyRecipe/ready_recipe/admin.py
+++ readyRecipe/ready_recipe/admin.py
@@ -1,17 +1,18 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
 
-
 from ready_recipe.models import Category,Recipe,Comment,UserProfile
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    prepolulated_fields = {'slug':('name')}
+    prepolulated_fields = {'slug': 'name'}
     list_display = ['name']
 
+
 class RecipeAdmin(admin.ModelAdmin):
-    prepolulated_fields = {'slug':('name')}
+    prepolulated_fields = {'slug': 'name'}
     list_display = ['name','picture','portions','difficulty','category_id','owner_id']
+
 
 class CommentAdmin(admin.ModelAdmin):
     list_display = ['date','recommentations','recipe_id','owner_id']
@@ -20,6 +21,7 @@ class CommentAdmin(admin.ModelAdmin):
 
 #class UserProfileAdmin(admin.ModelAdmin):
   #  prepolulated_fields = {'slug':('first_name')}
+
 
 admin.site.register(Category,CategoryAdmin)
 admin.site.register(Recipe,RecipeAdmin)
